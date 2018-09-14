a8d5d927e7cec7152f00bb3165b4008783c4bbf4
gandolf <694792122@qq.com>
2018-09-14 15:56:05 +0800

    侧滑页

--- MyAndroid/app/src/main/java/com/yangys/activity/MainActivity.java
+++ MyAndroid/app/src/main/java/com/yangys/activity/MainActivity.java
@@ -1,5 +1,7 @@
 package com.yangys.activity;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,9 +9,19 @@ import example.com.myandroid.R;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView(){
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.navi_view);
     }
 }
